3f8d4d9f94c10e929441d2a8c95fffb2ad8295e0
Sherif M <126361282+mohams94@users.noreply.github.com>
2023-10-18 18:31:49 +0200

    Update mm_avalon_sqrt.vhd

--- vhdl/sqrt/mm_avalon_sqrt.vhd
+++ vhdl/sqrt/mm_avalon_sqrt.vhd
@@ -25,6 +25,101 @@ end entity;
 
 
 architecture rtl of avalon_mm_sqrt is
-begin
-end architecture;
 
+	component alt_fwft_fifo is
+		generic (
+			DATA_WIDTH : integer := 32;
+			NUM_ELEMENTS : integer 
+			);
+		port (
+			aclr		: IN STD_LOGIC ;
+			clock		: IN STD_LOGIC ;
+			data		: IN STD_LOGIC_VECTOR (DATA_WIDTH-1 DOWNTO 0);
+			rdreq		: IN STD_LOGIC ;
+			wrreq		: IN STD_LOGIC ;
+			empty		: OUT STD_LOGIC ;
+			full		: OUT STD_LOGIC ;
+			q		: OUT STD_LOGIC_VECTOR (DATA_WIDTH-1 DOWNTO 0)
+			);
+	end component;
+	
+	-- signals for the components
+	signal fifo_q, fifo_data, sqrt_result, sqrt_input, sqrt_remainder : std_logic_vector(31 downto 0);
+	signal fifo_empty, fifo_full, fifo_rd, fifo_wr : std_logic;
+	
+	-- constant for setting the number of pipeline stages
+	constant STAGES : integer := 16;
+
+begin
+    process (clk)
+    begin
+       if res_n = '0' then
+			fifo_data <= (others => '0');
+			fifo_q <= (others => '0');
+			sqrt_result <= (others => '0');
+			sqrt_input <= (others => '0');
+			sqrt_remainder <= (others => '0');
+			fifo_empty <= '0';
+			fifo_full <= '0';
+			fifo_rd <= '0';
+			fifo_wr <= '0';
+		 elsif rising_edge(clk) then
+            if write = '1' then
+					if address(0) = '0' then
+						sqrt_input <= writedata;
+					end if;
+                -- Write a new value to the module
+                -- Implement your FIFO write logic here
+            end if;
+            
+            if read = '1' then
+                -- Read the result, if data is available
+                if fifo_empty = '0' and address(0) = '0' then
+                    sqrt_result <= (others => 'X');  -- Undefined result
+                else
+                    --sqrt_result <= -- Implement your FIFO read logic here
+                end if;
+            end if;
+            
+            -- Determine if data is available to be read from the core
+            --data_available <= -- Implement your FIFO empty flag logic here
+        end if;
+    end process;
+
+    -- Connect ALTSQRT IP core to your design
+    -- Instantiate and connect the ALTSQRT IP core here
+
+    -- Output data based on the read address
+    readdata <= sqrt_result when read = '1' else (others => 'X');
+	 
+	 
+	sqrt: altsqrt
+	generic map(
+		pipeline => STAGES,
+		width => 32
+	)
+	port map(
+		aclr => res_n,
+		clk => clk,
+		q => sqrt_result,
+		radical => sqrt_input,
+		remainder => sqrt_remainder
+	);
+	
+	fifo : alt_fwft_fifo
+	generic map(DATA_WIDTH => 32,
+
+		NUM_ELEMENTS => 128)
+
+	port map(
+		aclr => res_n,
+		clock => clk,
+		data => sqrt_result,
+		rdreq => fifo_rd,
+		wrreq => fifo_wr,
+		empty => fifo_empty,
+		full => fifo_full,
+		q => fifo_q
+	);
+	 
+end architecture;
