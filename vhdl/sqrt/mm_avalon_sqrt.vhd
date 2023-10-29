45e00cae17831f2bffbbf68e5e944eac621c0e07
mohams94 <sherif3a@hotmail.com>
2023-10-30 00:24:59 +0100

    sqrt module working on hardware

--- vhdl/sqrt/mm_avalon_sqrt.vhd
+++ vhdl/sqrt/mm_avalon_sqrt.vhd
@@ -49,7 +49,9 @@ architecture rtl of avalon_mm_sqrt is
 	signal fifo_q, fifo_data : std_logic_vector(31 downto 0) := (others => '0');
 	signal sqrt_input : std_logic_vector(63 downto 0) := (others => '0');
 	signal sqrt_result : std_logic_vector(31 downto 0) := (others => '0');
-	signal fifo_empty, fifo_full, fifo_rd, fifo_wr : std_logic := '0';
+	signal fifo_full, fifo_rd, fifo_wr : std_logic := '0';
+	signal fifo_empty : std_logic := '1';
+	signal error_flag : std_logic := '0';
 	signal sqrt_remainder : std_logic_vector(32 downto 0) := (others => '0');
 	signal count, count_next : Integer := 0;
 	signal shift_reg : std_logic_vector(STAGES-1 downto 0) := (others => '0');
@@ -58,42 +60,50 @@ architecture rtl of avalon_mm_sqrt is
 	signal read_flag, write_flag, division_flag : std_logic := '0';
 
 begin
+
+	readdata <= (others => '0') when (address(0) ='0' and fifo_empty = '0') else
+				(fifo_q) when (address(0) = '1') else
+				(others => 'X');
+				
     process (all)
     begin
 			if rising_edge(clk) then
-				if res_n = '1' then
+				if res_n = '0' then
 					--fifo_q <= (others => '0');
 					sqrt_input <= (others => '0');
-					readdata <= (others => 'X');
+					--readdata <= (others => 'X');
 					division_flag <= '0';
 					fifo_rd <= '0';
 					read_flag <= '0';
 					fifo_wr <= '0';
 					shift_reg <= (others => '0');
+					error_flag <= '0';
+					--fifo_empty <= '1';
+					--fifo_full <= '0';
 				else
 					if write = '1' and address(0) = '0' then
-						sqrt_input <= x"000" & "00" & writedata & "00" & x"0000";
+						sqrt_input <= x"00000000" & writedata;--x"000" & "00" & writedata & "00" & x"0000";
 						division_flag <= '1';
+						error_flag <= '1';
 					else
 						division_flag <= '0';
+						error_flag <= '0';
 					end if;
 					
-					if read_flag = '1' then
+					if read = '1' and fifo_empty = '1' then
+						--readdata <= (others => 'X');
+					elsif read = '1' and fifo_empty = '0' and read_flag = '0' then
+						 read_flag <= '1';
+						 fifo_rd <= '1';
+--						 if address(0) = '0' then
+--							   readdata <= x"0000000" & "000" & fifo_empty;
+--						 else
+--							   readdata <= fifo_q;
+--						 end if;
+					else
 						read_flag <= '0';
 						fifo_rd <= '0';
 					end if;
-					
-					if read = '1' and fifo_empty = '0' and read_flag = '0' then
-						 read_flag <= '1';
-						 fifo_rd <= '1';
-						 if address(0) = '0' then
-							   readdata <= x"0000000" & "000" & fifo_empty;
-						 else
-							   readdata <= fifo_q;
-						 end if;
-					end if;
-					
-					
 					
 					if shift_reg(0) = '1' and fifo_full = '0' then
 						fifo_wr <= '1';
@@ -113,7 +123,7 @@ begin
 		R_PORT_WIDTH => 33
 	)
 	port map(
-		aclr => res_n,
+		--aclr => res_n,
 		clk => clk,
 		q => sqrt_result,
 		radical => sqrt_input,
@@ -126,7 +136,7 @@ begin
 		NUM_ELEMENTS => 128)
 
 	port map(
-		aclr => res_n,
+		aclr => '0',
 		clock => clk,
 		data => sqrt_result,
 		rdreq => fifo_rd,
